ffa4bdd95a37674e60b371c8cc87ef038eec4681
hyuyu1012 <hanhyeongu495@gmail.com>
2025-09-01 17:36:48 +0900

    countup-basic

---
+++ Countup/temp.js
@@ -0,0 +1,24 @@
+function countingType1(num){
+  const element = document.querySelector('#number1')
+  if(num == 0){
+    element.innerHTML = '0';
+  } else {
+    /* 입력한 숫자를 33번에 걸쳐 0부터 올림. */
+    const each = Math.ceil(num/33);
+    let time = 0
+    for(let i=0; i<=num; i+=each){
+      element.innerHTML = i;
+      console.log(i);
+      setTimeout(() => {
+        element.innerHTML = i;
+      }, 20*time);
+      /* 딱 떨어지지 않는 숫자를 마지막에 그 숫자로 만들어주기 위함 */
+      if(i+each>this.maxNum1){
+        setTimeout(() => {
+          element.innerHTML = num;
+        }, 20*(time+1));
+      }
+      time++;
+    }
+  }
+}
